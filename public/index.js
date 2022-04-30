aaa3b73129f3f1c462f60325ce00f311dab55e7a
ntworm <103321841+ntworm@users.noreply.github.com>
2022-04-30 20:03:33 +0100

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,74 @@
+// ||||||||||||||||||||||||||||| FXHASH TEMPLATE - HYDRA + P5|||||||||||||||||||||||||||||||||||||||||||||||||||||||//            
+// Author: nt_worm  ||||  Twitter: @nt_worm
+// This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.       //
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/.                       //
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+/////////////////////////////// CANVAS FUNCTIONS/ //////////////////////////////////
+let WIDTH;
+let HEIGHT;
+let hydracanvas = document.createElement('canvas')
+hydracanvas.width = 1500
+hydracanvas.height = 1500
+let hydra = new Hydra({detectAudio: false, canvas: hydracanvas})
+let p5graphics
+function setup (){
+  WIDTH = windowHeight + 10;
+  HEIGHT = windowWidth;
+  if (WIDTH>HEIGHT) HEIGHT = WIDTH
+  else WIDTH = HEIGHT
+  hydra.setResolution(WIDTH, HEIGHT);
+  mycanvas = createCanvas(WIDTH,HEIGHT, WEBGL)
+  p5graphics = createGraphics(WIDTH, HEIGHT)
+  frameRate(60)
+  rectMode(CENTER);
+}
+/////////////////////////////////////HYDRA CANVAS///////////////////////////////////
+// src(o0)
+// .modulateHue(src(o0).scale(4.01),1)
+// .layer(osc(1,0,1.2, 3).hue(1.8).kaleid(1)
+// .contrast(1).mask(shape(4,0.7,0.004)))
+// .add(shape(444, 0.2, 0, 0.02).scrollX(0.9,0).scale(1,0.9))
+// .add(shape(44, 0.2, 0, 0.05).scrollX(0.28,0)).scrollY(2.1, 0)
+// .blend(shape(4,-5.04, 4).scrollY(-12, 0, 2.2))
+// .scrollX(-1, 0)
+// .scrollY(-1.1, 0)
+// .pixelate(11,11)
+// .out();
+osc (10,10,10)
+.out (o0)
+////////////////////////////////P5 ANIMATION////////////////////////////////////////
+function draw() {
+  noStroke()
+  plane(WIDTH +10,HEIGHT+10)
+  p5graphics.drawingContext.drawImage(hydracanvas, 0, 0)
+  texture(p5graphics)
+  
+  
+}
+window.$fxhashFeatures = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
